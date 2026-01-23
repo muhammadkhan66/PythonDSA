a62b4314bc661ce4d2d83759f260f047b23e7611
Muhammad Abdul Rehman Khan <mabdulrehmankhan31@gmail.com>
2026-01-23 10:23:30 -0800

    Add files via upload

---
+++ non_constructible_change.py
@@ -0,0 +1,11 @@
+def nonConstructibleChange(coins):
+    coins.sort()
+
+    currentChangeCreated = 0
+    for coin in  coins:
+        if coin>currentChangeCreated + 1:
+            return currentChangeCreated +1
+        currentChangeCreated +=coin
+    return currentChangeCreated + 1
+
+print(nonConstructibleChange([1,2,3,4,5,6,7,19]))
